e597b57a69eaa5d4486e2b9d2b9e8a2b79621281
coolpyprogrammer <coolpythonprogrammer@gmail.com>
2026-01-15 22:08:30 +0530

    Fixed login bug.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import auth
 if auth.load_from_session():
     auth.load_from_session()
 else:
-    option = int("""1. Create account
-    2. Sign In""")
+    option = int(input("""1. Create account
+    2. Sign In"""))
 
     if option == 1:
         auth.user_sign_up()
